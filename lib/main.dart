bfb972a80c68073c99cfdbc0cea07d80ba006393
SAMIULLAH-KALHORO-DEV <samiullahabbasy@gmail.com>
2023-03-30 23:35:16 +0500

    UI Design Completed

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(brightness: Brightness.light),
       darkTheme: ThemeData(brightness: Brightness.dark),
-      themeMode: ThemeMode.dark,
+      themeMode: ThemeMode.system,
       home: const Calculator(),
     );
   }
